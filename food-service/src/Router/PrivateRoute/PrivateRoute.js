331b332a291ef8befb8bf84854f1d06f2ae5ef9c
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-11-09 16:02:57 +0600

    spinner-added

--- food-service/src/Router/PrivateRoute/PrivateRoute.js
+++ food-service/src/Router/PrivateRoute/PrivateRoute.js
@@ -1,12 +1,25 @@
 import React, { useContext } from 'react';
 import {  Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
+import { Circles } from  'react-loader-spinner'
+
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext)
     const location = useLocation();
     if(loading){
-        return <h2 className='text-5xl'>Loading....</h2>
+        return <div className='text-center flex align-items-center justify-center'>
+            <Circles
+        
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+        </div>
     }
     if(user){
         return children;
@@ -14,3 +27,6 @@ const PrivateRoute = ({children}) => {
     return <Navigate to="/login" state={{from: location}} replace></Navigate>
   }
   export default PrivateRoute;
+
+
+  
